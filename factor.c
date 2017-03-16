0dd891bbfd3d82e474127eac2b44a4218a34b2d7
David Zhao <david.zhao14@gmail.com>
2017-03-15 21:45:20 -0400

    first attempt. Not working"

--- factor.c
+++ factor.c
@@ -1,25 +1,43 @@
-
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
-
+#include <pthread.h>
 #include "queue.h"
 #include "factor.h"
+#include <stdio.h>
 
 // FIXME: Shared mutable data
+extern int DONE;
 static queue* iqueue;
 static queue* oqueue;
+pthread_mutex_t iqueue_mutex;
+pthread_mutex_t oqueue_mutex;
+pthread_cond_t iqueue_cond;
+pthread_cond_t oqueue_cond;
 
 void 
 factor_init()
 {
     if (iqueue == 0) iqueue = make_queue();
     if (oqueue == 0) oqueue = make_queue();
+	pthread_mutex_init(&iqueue_mutex, 0);
+	pthread_mutex_init(&oqueue_mutex, 0);
+	pthread_cond_init(&iqueue_cond, 0);
+	pthread_cond_init(&oqueue_cond, 0);
 }
 
 void
-factor_cleanup()
+factor_cleanup(int threads, pthread_t* workers)
 {
+	for (int i = 0; i < threads; i++) {
+		printf("gonna hang?\n");
+		int rv = pthread_join(workers[i], 0);
+		assert(rv == 0);
+	}
+	pthread_mutex_destroy(&iqueue_mutex);
+	pthread_mutex_destroy(&oqueue_mutex);
+	pthread_cond_destroy(&oqueue_cond);
+	pthread_cond_destroy(&iqueue_cond);
     free_queue(iqueue);
     iqueue = 0;
     free_queue(oqueue);
@@ -47,7 +65,6 @@ free_job(factor_job* job)
 void 
 submit_job(factor_job* job)
 {
-
     queue_put(iqueue, job);
 }
 
@@ -92,13 +109,22 @@ factor(int128_t xx)
 void 
 run_jobs()
 {
-    // FIXME: This should happen in its own thread.
+	//printf("thread: %d\n", (int)pthread_self());
     factor_job* job;
+	pthread_mutex_lock(&iqueue_mutex);
+	while (DONE == 0 && !(job = queue_get(iqueue))) {
+		printf("hang\n");
+		pthread_cond_wait(&iqueue_cond, &iqueue_mutex);
+	}
+	pthread_mutex_unlock(&iqueue_mutex);
+	if (DONE == 1) { return;}
+	
+	assert(job != 0);
 
-    // FIXME: We should block on an empty queue waiting for more work.
-    //        We can still use null job as the "end" signal.
-    while ((job = queue_get(iqueue))) {
-        job->factors = factor(job->number);
-        queue_put(oqueue, job);
-    }
+    job->factors = factor(job->number);
+
+	pthread_mutex_lock(&oqueue_mutex);
+    queue_put(oqueue, job);
+	pthread_cond_broadcast(&oqueue_cond);
+	pthread_mutex_unlock(&oqueue_mutex);
 }
--- main.c
+++ main.c
@@ -1,15 +1,28 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-
+#include <pthread.h>
 #include "int128.h"
 #include "factor.h"
 #include "ivec.h"
 
+int DONE = 0;
+extern pthread_mutex_t oqueue_mutex;
+extern pthread_mutex_t iqueue_mutex;
+extern pthread_cond_t iqueue_cond;
+extern pthread_cond_t oqueue_cond;
+
+void*
+factorize_work() {
+	while (DONE == 0) {
+		run_jobs();
+	}
+}
+
 int
 main(int argc, char* argv[])
 {
+	printf("main: %d\n", (int)pthread_self());
     if (argc != 4) {
         printf("Usage:\n");
         printf("  ./main threads start count\n");
@@ -17,28 +30,35 @@ main(int argc, char* argv[])
     }
 
     int threads = atoi(argv[1]);
-    assert(threads == 1);
+	pthread_t workers[threads];
+	for (int i = 0; i < threads; i++) {
+		int rv = pthread_create(&(workers[i]), 0, factorize_work, 0);
+		assert(rv==0);
+	}
 
     int128_t start = atoh(argv[2]);
     int64_t  count = atol(argv[3]);
 
     // FIXME: Maybe we're spawning threads in init?
     factor_init(); //creates two queues.
-	
 	//fill up iqueue
     for (int64_t ii = 0; ii < count; ++ii) {
         factor_job* job = make_job(start + ii);
+		pthread_mutex_lock(&iqueue_mutex);
         submit_job(job);
+		pthread_cond_broadcast(&iqueue_cond);
+		pthread_mutex_unlock(&iqueue_mutex);
     }
-
-    // FIXME: This should be (threads) seperate threads.
-    run_jobs();
 
     int64_t oks = 0;
 
+	factor_job* job;
     // FIXME: This should probably be while ((job = get_result()))
     for (int64_t ii = 0; ii < count; ++ii) {
-        factor_job* job = get_result();
+		pthread_mutex_lock(&oqueue_mutex);
+        while (!(job = get_result())) {
+			pthread_cond_wait(&oqueue_cond, &oqueue_mutex);
+		}
 
         print_int128(job->number);
         printf(": ");
@@ -60,12 +80,13 @@ main(int argc, char* argv[])
         }
 
         free_job(job);
+		pthread_mutex_unlock(&oqueue_mutex);
     }
 
+	DONE = 1;
     printf("Factored %ld / %ld numbers.\n", oks, count);
 
-    factor_cleanup();
-    // FIXME: We should have joined all spawned threads.
+    factor_cleanup(threads, workers);
 
     return 0;
 }
